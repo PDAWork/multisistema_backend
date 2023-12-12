cd36d9b9cc445cfde757e09e649926aa20185520
pda <pahomovdaniil02@yandex.ru>
2023-12-12 15:00:26 +0300

    Сделан запрос для object

--- controller/object.js
+++ controller/object.js
@@ -3,11 +3,17 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 
 /// Получить один объект но с сокращенной информацией
-function getObjectShortened(req, res) {}
+function getObjectShortened(req, res) { }
 
 /// Получить один объект но с полной информацией
-function getObjectFull(req, res) {
-  res.send(req.params["id"]);
+async function getObjectFull(req, res) {
+  const id = req.params.id;
+  const object = await model.Object.findOne({ where: { id: id } })
+  if (object === null) {
+    res.status(404).json();
+    return;
+  }
+  return res.status(200).json(object);
 }
 
 /// Получить все объекты пользователя и подробную информацию
@@ -36,8 +42,7 @@ async function getObjects(req, res) {
   });
 
   if (object.length === 0) {
-    const sid = user.sid;
-
+    const t = await model.sequelize.transaction();
     try {
       const resultUserObject = await getObjectSaurus(user);
       for (
@@ -46,78 +51,6 @@ async function getObjects(req, res) {
         index++
       ) {
         const object = resultUserObject.data.data.objects[index];
-        const result = await model.Object.create({
-          id: object.id,
-          house: object.house,
-          lable: object.label,
-          accountId: object.object_company_account,
-          personalAccount: object.personal_account,
-          connectDate: object.connect_dt,
-          enable: object.enable,
-          balanceObject: 0,
-          userId: userId,
-          accesLevel: object.access_level,
-          objectCompanyName: object.object_company_name,
-          objectCompanyUrl: object.object_company_url,
-        });
-      }
-
-      res.status(200).json(
-        object.findAll({
-          where: {
-            userId: userId,
-          },
-        })
-      );
-
-      return;
-    } catch (error) {
-      console.log(error);
-    }
-    res.status(404).json({
-      message: "У пользователя нет ни одного объект",
-    });
-    return;
-  }
-
-  let isCheck = false;
-  const currentDate = new Date();
-  const oneWeekInMillis = 7 * 24 * 60 * 60 * 1000;
-
-  for (let index = 0; index < object.length; index++) {
-    console.log(object[index].updatedAt);
-    const updateDate = new Date(object[index].updatedAt);
-    const timeDifference = currentDate.getTime() - updateDate.getTime();
-    if ((isCheck = timeDifference > oneWeekInMillis)) {
-      break;
-    }
-    console.log(isCheck);
-  }
-
-  if (isCheck) {
-    const resultUserObject = await getObjectSaurus(user);
-    for (
-      let index = 0;
-      index < resultUserObject.data.data.objects.length;
-      index++
-    ) {
-      const object = resultUserObject.data.data.objects[index];
-      const result = await model.Object.update(
-        {
-          house: object.house,
-          lable: object.label,
-          accountId: object.object_company_account,
-          personalAccount: object.personal_account,
-          connectDate: object.connect_dt,
-          enable: object.enable,
-          accesLevel: object.access_level,
-          objectCompanyName: object.object_company_name,
-          objectCompanyUrl: object.object_company_url,
-        },
-        { where: { id: object.id } }
-      );
-
-      if (result == 0) {
         await model.Object.create({
           id: object.id,
           house: object.house,
@@ -131,17 +64,103 @@ async function getObjects(req, res) {
           accesLevel: object.access_level,
           objectCompanyName: object.object_company_name,
           objectCompanyUrl: object.object_company_url,
-        });
+        }, { transaction: t });
       }
+
+      await t.commit();
+
+      const objects = await model.Object.findAll({
+        where: {
+          userId: userId,
+        },
+      });
+
+      res.status(200).json(
+        objects
+      );
+      return;
+    } catch (error) {
+      await t.rollback();
+      res.status(500).json({
+        message: "Ошибка сервера",
+      });
+      return;
+    }
+  }
+
+  let isCheck = false;
+  const currentDate = new Date();
+  const oneWeekInMillis = 7 * 24 * 60 * 60 * 1000;
+
+  for (let index = 0; index < object.length; index++) {
+    console.log(object[index].updatedAt);
+    const updateDate = new Date(object[index].updatedAt);
+    const timeDifference = currentDate.getTime() - updateDate.getTime();
+    if ((isCheck = timeDifference > oneWeekInMillis)) {
+      break;
+    }
+  }
+
+  if (isCheck) {
+    const t = await model.sequelize.transaction();
+    try {
+
+      const resultUserObject = await getObjectSaurus(user);
+      for (
+        let index = 0;
+        index < resultUserObject.data.data.objects.length;
+        index++
+      ) {
+        const object = resultUserObject.data.data.objects[index];
+        const result = await model.Object.update(
+          {
+            house: object.house,
+            lable: object.label,
+            accountId: object.object_company_account,
+            personalAccount: object.personal_account,
+            connectDate: object.connect_dt,
+            enable: object.enable,
+            accesLevel: object.access_level,
+            objectCompanyName: object.object_company_name,
+            objectCompanyUrl: object.object_company_url,
+          },
+          { where: { id: object.id } }, { transaction: t }
+        );
+
+        if (result == 0) {
+          await model.Object.create({
+            id: object.id,
+            house: object.house,
+            lable: object.label,
+            accountId: object.object_company_account,
+            personalAccount: object.personal_account,
+            connectDate: object.connect_dt,
+            enable: object.enable,
+            balanceObject: 0,
+            userId: userId,
+            accesLevel: object.access_level,
+            objectCompanyName: object.object_company_name,
+            objectCompanyUrl: object.object_company_url,
+          }, { transaction: t });
+        }
+
+      }
+      t.commit();
+      const object = await model.Object.findAll({
+        where: {
+          userId: userId,
+        },
+      });
+      res.status(200).json(object);
+      return;
+    } catch (error) {
+      await t.rollback();
+      res.status(500).json({
+        message: "Ошибка сервера",
+      });
+      return;
     }
 
-    const object = await model.Object.findAll({
-      where: {
-        userId: userId,
-      },
-    });
-    res.status(200).json(object);
-    return;
   }
 
   if (object.length != 0) {
