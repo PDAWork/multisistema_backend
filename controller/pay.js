f59d6f12fe9550a32be04c545a4d9a9ed4994f85
PDAWork <pahomovdaniil02@yandex.ru>
2024-01-09 08:24:21 +0300

    Pay

---
+++ controller/pay.js
@@ -0,0 +1,19 @@
+const model = require("../db/models/index");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+
+
+async function pay() {
+
+}
+
+async function getAllTatiff() {
+}
+
+async function getTariffObject() {
+
+}
+
+async function getAllTariffObject() {
+    
+}
