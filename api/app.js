8d9df8b0ec1aa046981e613b3877278243af3d6d
PDAWork <pahomovdaniil02@yandex.ru>
2024-03-05 13:12:09 +0300

    Была изменена структура на микро сервисы теперь есть сервис который отвечает за api и сервис который отвечает за push

---
+++ api/app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const authRoute = require("./routes/auth");
+const objectRoute = require("./routes/object");
+const meter = require("./routes/meter");
+const payRouter = require("./routes/pay");
+const app = express();
+const jwt = require(`jsonwebtoken`);
+const model = require('./db/models/index');
+const compression = require('compression');
+
+app.use(express.json());
+app.use(compression());
+
+app.use((req, res, next) => {
+    let log = {
+        method: req.method || "GET",
+        host: req.host || "localhost",
+        port: req.port || "443",
+        path: req.pathname || req.path || "/",
+        body: req.body,
+        // headers: req.headers || {},
+    };
+    console.log(log);
+    next();
+});
+
+app.get("/", (req, res) => {
+    return res.send( "<h1>Hello world</h1>");
+})
+app.use("/api/auth", authRoute);
+app.use(
+    "/api/object",
+    (req, res, next) => {
+        authenticateToken(req, res, next);
+    },
+    objectRoute
+);
+app.use(
+    "/api/meter",
+    meter
+);
+
+
+app.use(
+    "/api/pay",
+    (req, res, next) => {
+        if (req.path == "/webhook") {
+            next();
+            return;
+        }
+        authenticateToken(req, res, next);
+    },
+    payRouter
+)
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    console.log(authHeader);
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null)
+        return res
+            .status(401)
+            .json({message: "Пользователь не авторизован"})
+            .end();
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        console.log(err);
+        if (err) {
+            if (err.name === "TokenExpiredError") {
+                return res.status(403).json({message: "Время действия токена истекло"});
+            } else {
+                return res.status(403).json({message: "Неверный токен"});
+            }
+        }
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = app;
---
+++ api/server.js
@@ -0,0 +1,47 @@
+const app = require("./app");
+const http = require("http").createServer(app),
+    io = require('socket.io')(http);
+const model = require("./db/models/index");
+require("dotenv").config();
+
+const port = process.env.PORT_API || 3000;
+http.listen(port, (err) => {
+    if (err) {
+        console.log(`Ошибка сервера ${err}`)
+    }
+    model.orders.afterBulkCreate((order, options) => {
+        io.on(order.idOrder, (socket) => {
+            console.log(`Client with id ${socket.id} connect`);
+        });
+    })
+    model.orders.afterBulkUpdate(async (order, options) => {
+
+        io.emit(order.where.idOrder, order.attributes.status);
+        var orderQuery = await model.orders.findOne(
+            {
+                where: {
+                    idOrder: order.where.idOrder
+                }
+            }
+        )
+        const startDate = new Date();
+        const finishDate = new Date().setFullYear(startDate.getFullYear() + 1);
+        model.tariffObject.update(
+            {
+                objectId: orderQuery.objectId,
+                tariffId: orderQuery.tariffId,
+                startDate: startDate,
+                finishDate: finishDate
+
+            },
+            {
+                where: {
+                    objectId: orderQuery.objectId,
+                },
+            },
+        )
+    });
+
+    console.log(`Сервер запущен на ${port}`)
+})
+;
